9b9eed1aaf52fff00aa3a3840791d085db319536
woodylan <woodylan@126.com>
2019-10-29 23:39:40 +0800

    user-defined port

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"go-websocket/src"
+	"os"
 )
 
 func main() {
-	server := src.NewServer(":666")
+	port := getPort()
+	server := src.NewServer(":" + port)
+
+	fmt.Printf("服务器启动成功，端口号：%s\n", port)
 
 	if err := server.ListenAndServer(); err != nil {
 		panic(err)
 	}
+}
+
+func getPort() string {
+	port := "666"
+
+	args := os.Args //获取用户输入的所有参数
+	if args != nil && len(args) >= 2 && len(args[1]) != 0 {
+		port = args[1];
+	}
+
+	return port
 }
